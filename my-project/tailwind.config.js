37ea45330fa226b8b4a6476d3840cfe1f6297adc
bharathikannansambasivam <bharathikannansambasivam@gmail.com>
2024-12-15 21:32:57 +0530

    feat: PWA feature added

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -2,12 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      transform: {
-        "rotate-y-90": "rotateY(90deg)",
-        "rotate-y-0": "rotateY(0deg)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      register: "autoUpdate",
+      manifest: {
+        name: "Memory Game",
+        short_name: "Memory Game",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#FFFFFF",
+        theme_color: "#FFFFFF",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
